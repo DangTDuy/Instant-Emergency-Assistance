39453af5610acff88a4c38d9702f0eb0d212bd55
huynh-dang187 <112720090+huynh-dang187@users.noreply.github.com>
2025-04-19 00:01:33 +0700

    Update Again

--- AppResQnow/app/src/main/java/com/example/resqnow/Ui_Ux/theme/contact/ContactScreen.kt
+++ AppResQnow/app/src/main/java/com/example/resqnow/Ui_Ux/theme/contact/ContactScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -176,13 +177,14 @@ fun ContactScreen(
 
         ) { paddingValues ->
         Column(
+
             modifier = Modifier
                 .padding(paddingValues)
         ) {
-            // HeadBar luôn hiển thị ở trên
+
             HeadBar()
 
-        if (contacts.isEmpty()) {
+            if (contacts.isEmpty()) {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
@@ -374,6 +376,7 @@ fun HeadBar() {
         contentAlignment = Alignment.Center,
         modifier = Modifier
             .fillMaxWidth()
+            .statusBarsPadding()
             .height(91.dp)
     ) {
         Image(
